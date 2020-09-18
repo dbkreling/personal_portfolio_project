74c90d3ba22e22f76ae3e994b4b8a392707bbbac
Daniel Kreling <dbkreling@gmail.com>
2020-09-18 15:04:10 +0200

    blog/models.py: change type of description field
    Change from CharField (which is mandatory) to TextField, and allow bigger texts.
    
    Reference:
    https://docs.djangoproject.com/en/3.1/ref/models/fields/#django.db.models.TextField
    
    Signed-off-by: Daniel Kreling <dbkreling@gmail.com>

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
+    description = models.TextField(max_length=5000)
     url = models.URLField(blank=True)
     date = models.DateField()
 
